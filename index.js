1a9bdc9908d235261b31db5a64a4d8db49ede9bf
Rachel Wyatt <rw126@evansville.edu>
2019-04-12 14:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function nameToAttributes (drivers) {
 function attributesToPhrase (drivers) {
   return drivers.map(function (driver {
     return ${driver["name"]} is from ${driver["hometown"]}
-  }))
+  });)
 }
