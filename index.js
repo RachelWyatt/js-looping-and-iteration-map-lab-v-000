7ca582bc38da80b1562d35e91e2c7d5e67cd68de
Rachel Wyatt <rw126@evansville.edu>
2019-04-12 14:46:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file.
 function lowerCaseDrivers (drivers) {
-  const lowerCase = drivers.map(name => name.toLowerCase90);
+  const lowerCase = drivers.map(name => name.toLowerCase);
   return lowerCase
 }
